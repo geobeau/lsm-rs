2e6e49a10f370da1b59f7d162212fd38053398f8
Geoffrey Beausire <g.beausire@criteo.com>
2023-03-10 16:04:14 +0100

    Add benchmark for single queries

--- benches/kvs/main.rs
+++ benches/kvs/main.rs
@@ -8,7 +8,7 @@ use rand::{distributions::Alphanumeric, thread_rng, Rng};
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn kvs_benchmark(c: &mut Criterion) {
+pub fn kvs_big_dataset_benchmark(c: &mut Criterion) {
     let dataset_10k = gen_dataset(10_000);
     let config = Config {
         memtable_max_size_bytes: 4096,
@@ -25,6 +25,18 @@ pub fn kvs_benchmark(c: &mut Criterion) {
         disktable_target_usage_ratio: 0.7,
     };
     c.bench_function("kvs: insert random 10K, 64MiB tables", |b| b.iter(|| kvs_insert(&dataset_10k, &config)));
+}
+
+pub fn kvs_insert_dataset_benchmark(c: &mut Criterion) {
+    let config = Config {
+        memtable_max_size_bytes: 4 * 1024 * 1024,
+        disktable_target_usage_ratio: 0.7,
+    };
+    let mut d = datastore::DataStore::new_with_config(PathBuf::from("./data/bench/"), config.clone());
+    d.truncate();
+    c.bench_function("kvs: insert, 4kiB tables", |b| b.iter(|| {
+        d.set(Record::new(gen_string(), gen_string()))
+    }));
 }
 
 fn gen_string() -> String {
@@ -50,5 +62,6 @@ fn kvs_insert(dataset: &[(String, String)], conf: &Config) {
     s.truncate();
 }
 
-criterion_group!(benches, kvs_benchmark);
+// criterion_group!(benches, kvs_big_dataset_benchmark, kvs_insert_dataset_benchmark);
+criterion_group!(benches, kvs_insert_dataset_benchmark);
 criterion_main!(benches);
