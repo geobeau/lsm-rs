57edbf0fba6f6acbb5c89138aaf4561a4aef3cf4
Geoffrey Beausire <geobeausire@gmail.com>
2023-05-05 15:00:53 +0200

    Working version of memcached server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use glommio::{net::TcpListener, prelude::*};
-use lsm_rs::{datastore::DataStore, memcached::MemcachedBinaryHandler, record::Record};
+use lsm_rs::{datastore::DataStore, memcached::{MemcachedBinaryHandler, server::MemcachedBinaryServer, Get, GetResp, SetResp, Set}, record::Record};
+
+fn handle_get(g: Get) -> GetResp {
+    return GetResp {
+        flags: 0,
+    }
+}
+
+fn handle_set(s: Set) -> SetResp {
+    return SetResp {
+        opcode: lsm_rs::memcached::OpCode::NoError,
+        cas: 0,
+    }
+}
+
+
 
 fn main() {
     let ex = LocalExecutorBuilder::new(Placement::Fixed(0)).make().unwrap();
@@ -9,16 +24,14 @@ fn main() {
         println!("{:?}", s.get("test").await.unwrap());
     });
 
+
+
     ex.run(async move {
-        let listener = TcpListener::bind("127.0.0.1:11211").unwrap();
-        println!("Listening on {}", listener.local_addr().unwrap());
-        let stream = listener.accept().await.unwrap();
-        let mut handler = MemcachedBinaryHandler { reader: stream };
-        let command = handler.decode_command().await;
-        println!("{:?}", command);
-        let stream = listener.accept().await.unwrap();
-        let mut handler = MemcachedBinaryHandler { reader: stream };
-        let command = handler.decode_command().await;
-        println!("{:?}", command);
+        let s = MemcachedBinaryServer {
+            host_port: "127.0.0.1:11211".to_string(),
+            get_handler: handle_get,
+            set_handler: handle_set,
+        };
+        s.listen().await;
     });
 }
